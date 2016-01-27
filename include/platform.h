e2f997a86cb9fc7cbd47861f65bc70d8dc6a9a9b
Andrew Mundy <andrew.mundy@ieee.org>
2016-01-27 13:22:59 +0000

    Fix example, aid debugging

--- include/platform.h
+++ include/platform.h
@@ -7,12 +7,14 @@ void * safe_malloc(uint bytes)
   void* p = spin1_malloc(bytes);
   if (p == NULL)
   {
+    io_printf(IO_BUF, "Failed to malloc %u bytes.\n", bytes);
     rt_error(RTE_MALLOC);
   }
   return p;
 }
 
   #define MALLOC safe_malloc
+
   #define FREE   sark_free
 #else
   #include <stdlib.h>
