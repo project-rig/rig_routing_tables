3267d0ce616fb16968054f289ffb594a12737b86
Andrew Mundy <andrew.mundy@ieee.org>
2016-06-08 21:30:41 +0100

    fixup! Add tools for reading/writing routing tables to file

--- py_tests/test_utils.py
+++ py_tests/test_utils.py
@@ -1,6 +1,9 @@
+import pytest
 from rig.routing_table import RoutingTableEntry as RTE
 from rig.routing_table import Routes
 from rig_routing_tables import utils
+import struct
+import tempfile
 
 
 def test_table_to_cffi_and_back():
@@ -42,3 +45,107 @@ def test_table_to_cffi_and_back():
     # Convert back again
     returned_table = utils.c_to_rig_table(c_table)
     assert table == returned_table
+
+
+def test_read_routing_table_from_file():
+    """Test reading a routing table from file."""
+    # Write two routing tables to file in the appropriate format, then read
+    # back and ensure the result is correct.
+    fp = tempfile.TemporaryFile()
+
+    # Write the first routing table:
+    # (1, 2):
+    #     S -> 00000000000000000000000000000000 -> NE 3
+    #     E -> 00000000000000000000000000000001 -> S
+    fp.write(struct.pack("<2BH", 1, 2, 2))
+    fp.write(struct.pack("<4I", 0x0, 0xffffffff, 0b100000, 0b1000000010))
+    fp.write(struct.pack("<4I", 0x1, 0xffffffff, 0b000001, 0b0000100000))
+
+    # Write the second routing table:
+    # (0, 0):
+    #     ? -> 00000000000000000000000000000000 -> 1
+    fp.write(struct.pack("<2BH", 0, 0, 1))
+    fp.write(struct.pack("<4I", 0x0, 0xffffffff, 1 << 31, 0b10000000))
+
+    # Check that the table is expected
+    fp.seek(0)
+    assert dict(utils.read_routing_tables(fp)) == {
+        (1, 2): [
+            RTE({Routes.north_east, Routes.core(3)}, 0x0, 0xffffffff,
+                {Routes.south}),
+            RTE({Routes.south}, 0x1, 0xffffffff, {Routes.east}),
+        ],
+        (0, 0): [
+            RTE({Routes.core(1)}, 0x0, 0xffffffff),
+        ],
+    }
+
+    fp.close()
+
+
+def test_read_routing_table_from_file_truncated_header():
+    fp = tempfile.TemporaryFile()
+
+    # Write the first routing table:
+    # (1, 2):
+    #     S -> 00000000000000000000000000000000 -> NE 3
+    #     E -> 00000000000000000000000000000001 -> S
+    fp.write(struct.pack("<2BH", 1, 2, 2))
+    fp.write(struct.pack("<4I", 0x0, 0xffffffff, 0b100000, 0b1000000010))
+    fp.write(struct.pack("<4I", 0x1, 0xffffffff, 0b000001, 0b0000100000))
+
+    # Write a truncated header
+    fp.write(struct.pack("<2B", 0, 0))
+
+    # Check that an exception is raised
+    fp.seek(0)
+    with pytest.raises(AssertionError):
+        dict(utils.read_routing_tables(fp))
+
+    fp.close()
+
+
+def test_read_routing_table_from_file_truncated_entry():
+    fp = tempfile.TemporaryFile()
+
+    # Write the first routing table:
+    # (1, 2):
+    #     S -> 00000000000000000000000000000000 -> NE 3
+    #     E -> 00000000000000000000000000000001 -> S
+    fp.write(struct.pack("<2BH", 1, 2, 2))
+    fp.write(struct.pack("<4I", 0x0, 0xffffffff, 0b100000, 0b1000000010))
+    fp.write(struct.pack("<4I", 0x1, 0xffffffff, 0b000001, 0b0000100000))
+
+    # Write the second routing table (truncated)
+    # (0, 0):
+    #     ? -> 00000000000000000000000000000000 -> 1
+    fp.write(struct.pack("<2BHI", 0, 0, 1, 0))
+
+    # Check that an exception is raised
+    fp.seek(0)
+    with pytest.raises(AssertionError):
+        dict(utils.read_routing_tables(fp))
+
+    fp.close()
+
+
+def test_write_routing_tables():
+    """Write to file and read back to check the table is correct."""
+    tables = {
+        (0, 0): [
+            RTE({Routes.south, Routes.south_west}, 0x0, 0xf, {Routes.north}),
+            RTE({Routes.south, Routes.south}, 0x1, 0xf, {Routes.north_east}),
+            RTE({Routes.north}, 0x2, 0xe),
+        ],
+        (0, 1): [
+            RTE({Routes.north}, 0x2, 0xe),
+        ]
+    }
+
+    # Check on writing and reading
+    fp = tempfile.TemporaryFile()
+    utils.write_routing_tables(tables, fp)
+    fp.seek(0)
+    assert dict(utils.read_routing_tables(fp)) == tables
+
+    fp.close()
--- rig_routing_tables/utils.py
+++ rig_routing_tables/utils.py
@@ -1,6 +1,8 @@
 """Utilities for converting routing tables between different formats."""
 from rig.routing_table import RoutingTableEntry, Routes
 from _rig_routing_tables import ffi, lib
+from six import iteritems
+import struct
 
 
 def rig_to_c_table(table):
@@ -75,3 +77,113 @@ def c_to_rig_table(c_table):
                                        entry.keymask.mask, source))
 
     return table
+
+
+def read_routing_tables(fp):
+    """Read routing tables from a file.
+
+    Parameters
+    ----------
+    fp : file object
+        File object containing routing tables.
+
+    Each routing table starts with two bytes representing the x and y
+    co-ordinates of the chip to which the routing table relates (a byte for
+    each of the co-ordinates) and a short (two bytes) representing the number
+    of entries in the following table. A table entry consists of four words,
+    one each for the key, the mask, the source and the route fields.
+
+    Yields
+    ------
+    (x, y)
+        Co-ordinates of the routing table.
+    [:py:class:`rig.routing_table.RoutingTableEntry`, ...]
+        Routing table found at this co-ordinate.
+    """
+    for xy, c_table in read_routing_tables_in_c_format(fp):
+        yield xy, c_to_rig_table(c_table)
+
+
+def write_routing_tables(tables, fp):
+    """Write routing tables to a file.
+
+    Parameters
+    ----------
+    tables : {(x, y): [:py:class:`rig.routing_table.RoutingTableEntry`, ...]}
+        Dictionary mapping chip co-ordinates to the routing table associated
+        with that chip.
+    fp : file object
+    """
+    write_routing_tables_from_c_format(
+        {chip: rig_to_c_table(table) for chip, table in iteritems(tables)}, fp
+    )
+
+
+def read_routing_tables_in_c_format(fp):
+    """Read routing tables from a file.
+
+    Parameters
+    ----------
+    fp : file object
+        File object containing routing tables.
+
+    Yields
+    ------
+    (x, y)
+        Co-ordinates of the routing table.
+    c_table
+        Routing table found at this co-ordinate (in C format).
+    """
+    while True:
+        # Try to read the header
+        header = fp.read(4)
+        if header == b'':
+            break
+        assert len(header) == 4, "Truncated header"
+
+        x, y, length = struct.unpack("<2BH", header)
+
+        # Create the routing table
+        c_table_raw = lib.new_table(length)
+        assert c_table_raw != ffi.NULL
+        c_table = ffi.gc(c_table_raw, lib.free_table)
+
+        # Read in the entries
+        for entry in (c_table.entries[i] for i in range(length)):
+            data = fp.read(16)
+            assert len(data) == 16, "Truncated entry"
+            (entry.keymask.key, entry.keymask.mask,
+             entry.source, entry.route) = struct.unpack("<4I", data)
+
+        yield (x, y), c_table
+
+
+def write_routing_tables_from_c_format(tables, fp):
+    """Write routing tables to a file.
+
+    Parameters
+    ----------
+    tables : {(x, y): c_table}
+        Dictionary mapping chip co-ordinates to the routing table associated
+        with that chip.
+    fp : file object
+    """
+    # Write each table in turn, buffering in memory to save writes
+    for (x, y), table in iteritems(tables):
+        # Create the buffer
+        data = bytearray(4 + 16*table.size)
+
+        # Write the header
+        struct.pack_into("<2BH", data, 0, x, y, table.size)
+
+        # Dump in the entries
+        for i in range(table.size):
+            entry = table.entries[i]
+            struct.pack_into("<4I", data, 4 + 16*i,
+                             entry.keymask.key,
+                             entry.keymask.mask,
+                             entry.source,
+                             entry.route)
+
+        # Write out
+        fp.write(data)
